82d0ac44e55f24128a4c7ea3c771ded1b9183863
Timo Mämecke <timo@maemecke.com>
2014-12-08 02:05:13 +0100

    I hate it when i forget this

--- header.php
+++ header.php
@@ -10,6 +10,16 @@
 	<?php wp_head(); ?>
 </head>
 <body class="preload">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-35040830-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?php get_template_part( 'partial', 'svgmap' ); ?>
 <header class="b-header">
 	<a href="<?= home_url(); ?>">
